919d23ccd4c85c8c1a4d721fe6e8b62c4c001131
sadashivagnihotri <63258676+sadashivagnihotri@users.noreply.github.com>
2020-04-29 23:43:45 +0530

    Update test.java

--- shark/src/main/java/test.java
+++ shark/src/main/java/test.java
@@ -2,7 +2,6 @@ public class Test{
 
 	public static void main (String... args){
 		System.out.println("Welcome to Git Subversioning");
-		System.out.println("Welcome to Git Subversioning2");
 	}
 
 }
